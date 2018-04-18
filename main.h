90ddda82c7b4af9692cbce505a4d614146b50d20
jensenrrr <32421710+jensenrrr@users.noreply.github.com>
2018-04-17 22:06:11 -0400

    Update main.h

--- main.h
+++ main.h
@@ -41,7 +41,7 @@ class LinkedNode{
 	Node* getNeighbor();
 	LinkedNode* getNext();
 	void setNext(LinkedNode* next);
-	LinkedNode(double distance, Node* neighbor, LinkedNode* next);
+	LinkedNode(double distance, Node* neighbor);
 };
 
 
